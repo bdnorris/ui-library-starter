af98348aaf88bbd096a25cef7fc4e8e7e4c9cbdf
Brian Norris <bnorris@marlinnetwork.com>
2021-03-02 15:36:07 -0600

    eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setTemplateFormats([
-		'njk'
-	])
+		'njk',
+		'md',
+		'html',
+		'css',
+		'jpeg',
+		'jpg',
+		'png',
+		'svg',
+		'woff',
+		'woff2'
+	]);
+	eleventyConfig.addPassthroughCopy('static');
 	return {
 		dir: {
 			input: "views",
+			includes: "views/_includes",
 			output: "dist",
 		},
 	}
