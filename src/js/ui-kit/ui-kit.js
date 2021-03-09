7f3ef3edffdced98388d357cb1747d66215e5cfe
Brian Norris <bnorris@marlinnetwork.com>
2021-03-09 17:14:23 -0600

    builds

--- src/js/ui-kit/ui-kit.js
+++ src/js/ui-kit/ui-kit.js
@@ -1,3 +1,12 @@
 // Scripts for UI-Kit Site, not for you library
 
 console.log('Hello from the UI Kit')
+
+const panelControl = document.querySelector('.panel__control')
+const panel = document.querySelector('.panel')
+
+if (panelControl && panel) {
+	panelControl.addEventListener('click', () => {
+		panel.classList.toggle('active');
+	})
+}
